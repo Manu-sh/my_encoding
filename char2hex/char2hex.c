a986371b569f4840ad9c09ad68e5f84433beb819
Manu-sh <s3gmentationfault@gmail.com>
2020-08-06 13:31:50 +0200

    Update char2hex.c

--- char2hex/char2hex.c
+++ char2hex/char2hex.c
@@ -25,20 +25,20 @@ bool binary_to_hex(void *dst, const char *src, size_t src_len, bool use_uppercas
 #endif
 
 // (0..20).each {|i| p i % 16 === i & 15 }
-bool binary_to_hex(void *dst, const char *src, size_t src_len, bool use_uppercase) {
+bool binary_to_hex(void *dst, const char *src, uint_fast32_t src_len, bool use_uppercase) {
 
-	/* lookup table */
-	static const char b16_uppercase[] = "0123456789ABCDEF";
-	static const char b16_lowercase[] = "0123456789abcdef";
+    /* lookup table */
+    static const char b16_uppercase[] = "0123456789ABCDEF";
+    static const char b16_lowercase[] = "0123456789abcdef";
 
-	register const char *const b16 = use_uppercase ? b16_uppercase : b16_lowercase;
-    	register char *out = (char *)dst;
+    const char *const b16 = use_uppercase ? b16_uppercase : b16_lowercase;
+    char *out = (char *)dst;
 
-	for (size_t i = 0; i < src_len; i++, out += 2) {
-		uint8_t ch = src[i];
-		out[0] = b16[ ch >> 4 ];
-		out[1] = b16[ ch & 15 ];
-	}
+    for (uint_fast32_t i = 0; i < src_len; i++, out += 2) {
+        uint_fast8_t ch = src[i];
+        out[0] = b16[ ch >> 4 ];
+        out[1] = b16[ ch & 15 ];
+    }
 
-	return true;
+    return true;
 }
